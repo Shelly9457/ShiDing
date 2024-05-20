ac66936a924195d8491e381b4d6eb56783aed92a
Shelly9457 <96901490+Shelly9457@users.noreply.github.com>
2024-05-20 22:19:02 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,74 +1,13 @@
-
-const app = Vue.createApp({
+const Talisman_app = Vue.createApp({
     data() {
         return {
-            about: about_data,
-            information: information_data,
-            question: question_data,
-            is_show: false,
-            inputMessage: "",
-            robot_message: [
-            ],
+            Talisman_datas: Talisman_data,
+            Talisman_choose_datas: Talisman_choose_data,
+            Type_talismans: Type_talisman,
         }
-    },
-    computed: {
-        toggleStyle() {
-            return {
-                "transform": this.is_show ? "scale(1)" : "scale(0)"
-            }
-        },
-        getMessageLen() {
-            return this.robot_message ? true : false
-        }
-
     },
     methods: {
-        toggle() {
-            this.is_show = !this.is_show
-        },
-        question_ans(e) {
-            this.inputMessage = e.target.innerHTML
-            this.send_ans()
-        },
-        keybord(e) {
-            if (e.key == "Enter") this.send_ans()
-        },
-        send_ans() {
-            if (this.inputMessage) {
-                let mes = this.inputMessage
-                this.robot_message.push({
-                    message: mes,
-                    root: 'admin'
-                })
-                this.updata()
-                setTimeout(() => {
-                    this.robot_ans()
-                }, 500);
-            }
-
-        },
-        robot_ans() {
-            let mes = "您好，歡迎來到集順廟明德宮！更多問題請洽宮廟或來電(02-26633398)!"
-            this.question.forEach((item, index) => {
-                item.keyword.forEach((word) => {
-                    if (this.inputMessage.includes(word)) {
-                        return mes = this.question[index].ans
-                    }
-                })
-            });
-            this.robot_message.push({
-                message: mes,
-                root: "robot"
-            })
-            this.inputMessage = ""
-            this.updata()
-        },
-        updata() {
-            setTimeout(() => {
-                this.$refs.robotDiv.scrollTop = this.$refs.robotDiv.scrollHeight
-            },);
-
+        talisman_click() {
         }
-
     }
-}).mount("#app")
+}).mount("#Talisman_app")
