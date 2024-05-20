4b9339253404162a01bce4e4ed6cae366f0dafbb
Shelly9457 <96901490+Shelly9457@users.noreply.github.com>
2024-05-20 21:43:47 +0800

    Add files via upload

---
+++ Talisman/index.js
@@ -0,0 +1,9 @@
+const Talisman_app = Vue.createApp({
+    data() {
+        return {
+            Talisman_datas: Talisman_data,
+            Talisman_choose_datas: Talisman_choose_data,
+            Type_talismans: Type_talisman,
+        }
+    }
+}).mount("#Talisman_app")
