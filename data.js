c5aab52ef863009745f0c99abec485c736dee812
Shelly9457 <96901490+Shelly9457@users.noreply.github.com>
2024-05-20 22:21:22 +0800

    Add files via upload

--- data.js
+++ data.js
@@ -1,246 +1,107 @@
-const Talisman_data = [
+const about_data = [
     {
-        img: "./img/money.png"
+        id: "about_modal1",
+        title1: "廟宇介紹",
+        title2: "—TEMPLE INTRODUCTION—",
+        img: "./images/about1.png",
+        img2: "./images/about1-1.png",
+        text: [
+            "石碇集順廟，又稱雙忠廟、尪公廟、仙公廟等，是石碇地區居民的信仰中心，為當地最悠久的寺廟之一，也是石碇的重要地標。廟宇建立於清道光十八年（1838年），主祀雙忠(保儀尊王及保儀大夫)。",
+            "後於日治初期由中醫醫師「呂林烏木」見廟宇傾斜重建廟宇並建立二樓明德宮以，主祀呂純陽祖師（呂洞賓），由木柵指南宮分香而來。",
+            "集順廟在茶葉興盛的日治時期是茶葉交易中心，廟埕曾是石碇、坪林、平溪、深坑、宜蘭等鄉鎮居民交流的場所，具有重要的歷史和文化意義。"
+        ]
     },
     {
-        img: "./img/friend.png"
+        id: "about_modal2",
+        title1: "建築歷史",
+        title2: "—ARCHITECTURAL HISTORY—",
+        img: "./images/about2.png",
+        img2: "./images/about2-1.png",
+        text: [
+            "集順廟原創建於1838年整座建築以木構造為主，寺廟原建築以木頭構造為主，十分古色古香，但在歲月洗禮下，集順廟歷經多次修建，後於 1989 年改建為現今的兩層樓鋼筋混凝建築，改建後的廟貌華麗卻失去古意。",
+            "石碇位於新北市山區，因地理環境及交通因素限制，工商業並不發達，清康熙年多以農業為主。由於開墾過程艱辛，相傳，保儀尊王可以保祐開墾山坡地順利平安因此一樓供奉保儀尊王。",
+            "日治時期日本人以及台灣礦業鉅子合資在石碇設立「台北炭礦株式會社」，石碇居民原有的農耕生活型態開始了重大的改變，信仰也隨即改變。石碇成為北部煤礦產區，而呂洞賓則是被煤礦業者視為保護的神祇並在二樓明德宮供奉孚佑帝君（呂洞賓）。"
+        ]
     },
     {
-        img: "./img/love.png"
-    },
-    {
-        img: "./img/peo.png"
-    },
-    {
-        img: "./img/work.png"
-    },
-    {
-        img: "./img/health.png"
-    },
+        id: "about_modal3",
+        title1: "供奉神祇",
+        title2: "—FAITH AND WORSHIP—",
+        img: "./images/about3.png",
+        img2: "./images/about3-1.png",
+        text: [
+            "石一樓集順廟主祀雙忠(保儀尊王及保儀大夫) ，保儀尊王、保儀大夫二人為唐朝安史之亂時的張巡、許遠。唐朝安祿山造反時，許遠和張巡起兵討伐。一年，賊兵用十三萬大軍把尹子奇押在睢陽，張巡和許遠仍堅守孤城，但最後仍因饑餓而為國犧牲，於是大家便為這兩位忠臣建廟。後民間尊張巡為「保儀尊王」，許遠為「保儀大夫」。",
+            "集順廟的保儀尊王在唐末黃巢之亂時保護了高、張、林三姓，後來他們來台開墾時仍信奉此神，因此神威顯赫聲名遠播。",
+            "一樓主神龕前神桌供奉觀音佛祖、中壇元帥、司命灶君、福德正神，龍邊神龕奉祀關聖帝君，虎邊神龕奉祀天上聖母，兩側還有祀千里眼將軍及順風耳將軍。",
+            "二樓明德宮主祀孚佑帝君(呂洞賓) 即八仙裡的呂洞賓，又稱「純陽祖師」、「呂仙公」、「呂祖」等，亦簡稱仙公。屬於五文昌之一，是醫藥、製墨等行業的守護神，也常被煤礦業者視為保祐他們的神祇。",
+            "二樓主神龕前神桌供奉太上感應篇神位，龍邊神龕奉祀王靈天君，虎邊神龕奉祀張大帝。",
+        ]
+    }
 ]
-const Talisman_choose_data = [
+const information_data = [
     {
-        icon: "bi bi-house-heart",
-        text: "生活",
-        herf: "#life",
+        title: "購",
+        class: "order-1",
+        text: "選購一個平安符，依照平安符對應香爐標示顏色參拜(右圖)",
     },
     {
-        icon: "bi bi-book",
-        text: "事業/學業",
-        herf: "#work",
+        title: "香",
+        class: "order-2",
+        text: "點香，每樓各五炷",
     },
     {
-        icon: "bi bi-people",
-        text: "人際",
-        herf: "#friend",
+        title: "拜",
+        class: "order-3",
+        text: "依照香爐順序上香",
     },
     {
-        icon: "bi bi-heart",
-        text: "愛情",
-        herf: "#love",
-    },
-    {
-        icon: "bi bi-coin",
-        text: "財運",
-        herf: "#money",
-    },
-    {
-        icon: "bi bi-bandaid",
-        text: "健康",
-        herf: "#health",
+        title: "繞",
+        class: "order-5",
+        text: "最後以「∞」符號將平安符遶香爐三次",
     },
 ]
 
-const Type_talisman = [
+const question_data = [
     {
-        title: "生活",
-        href: "life",
-        contest: [
-            {
-                color: "藍色",
-                class: "blue",
-                text: "祈求未來生活一切順利"
-            },
-            {
-                color: "藍色",
-                class: "blue",
-                text: "祈求開(騎)車平安"
-            },
-            {
-                color: "藍色",
-                class: "blue",
-                text: "祈求神明給予心靈寄託"
-            },
-            {
-                color: "藍色",
-                class: "blue",
-                text: "讓過去的心理陰影消散去"
-            },
-            {
-                color: "藍色",
-                class: "blue",
-                text: "生活從負能量改變成正能量"
-            },
-            {
-                color: "藍色",
-                class: "blue",
-                text: "祈求未來生活一切順遂"
-            },
-            {
-                color: "黑色",
-                class: "black",
-                text: "心煩意亂想要擁有內心平靜與安寧"
-            },
-            {
-                color: "紅色",
-                class: "red",
-                text: "生活不順遂祈求能逢凶化吉"
-            },
-            {
-                color: "紅色",
-                class: "red",
-                text: "生活上擁有貴人的幫忙"
-            },
-            {
-                color: "橘色",
-                class: "organ",
-                text: "祈求夢想實現"
-            },
-            {
-                color: "橘色",
-                class: "organ",
-                text: "克服個人弱點與缺陷"
-            },
-            {
-                color: "橘色",
-                class: "organ",
-                text: "祈求擁有睿智、創作力、自信"
-            },
-        ]
+        ques: "請問集順廟是否提供「點光明燈」的服務？",
+        ans: "有福祿壽平安燈龍，請洽廟公或來電報名(02-26633398)<br>\
+            1.將於農曆新年期間開放有福祿壽平安燈龍活動<br>\
+            2.	有全家平安符燈籠(每盞1200元)以及個人平安燈籠(每盞600元)兩種形式。<br>\
+            3.	平安燈龍將全年掛置於廟堂大廳正前方，保有點燈著全家及個人元辰光彩，財運亨通，事業發達，學業進步，姻緣美滿，身體健康，諸事圓滿大吉祥。<br>\
+            4.	目前並沒有開放線上點燈的服務。",
+        keyword: ["光明燈", "平安燈籠"]
     },
     {
-        title: "事 業 / 學 業",
-        href: "work",
-        contest: [
-            {
-                color: "綠色",
-                class: "green",
-                text: "祈求學業蒸蒸日上"
-            },
-            {
-                color: "綠色",
-                class: "green",
-                text: "祈求事業順利成功"
-            },
-            {
-                color: "綠色",
-                class: "green",
-                text: "祈求升職加薪"
-            },
-            {
-                color: "綠色",
-                class: "green",
-                text: "祈求度過事業上的難關"
-            },
-            {
-                color: "綠色",
-                class: "green",
-                text: "事業上有貴人的幫助"
-            },
-            {
-                color: "綠色",
-                class: "green",
-                text: "擁有無限的機會"
-            },
-        ]
+        ques: "請問集順廟是否提供「安太歲」的服務?",
+        ans: "本廟有提供安太歲的服務，請犯太歲的本人到現場向主神參拜，可以跟點光明燈一同。",
+        keyword: ["安太歲"]
+    },
+
+    {
+        ques: "請問集順廟是否提供「收驚」的服務?",
+        ans: "本廟目前沒有提供此服務",
+        keyword: ["收驚"]
+    },
+
+    {
+        ques: "想參加集順廟的誦經班該如何報名呢?",
+        ans: "請洽廟公或來電報名(02 - 26633398)",
+        keyword: ["誦經班"]
     },
     {
-        title: "人際",
-        href: "fiend",
-        contest: [
-            {
-                color: "黑色",
-                class: "black",
-                text: "人際關係出現爭吵想要和平解決"
-            },
-            {
-                color: "黑色",
-                class: "black",
-                text: "生活中出現小人"
-            },
-            {
-                color: "橘色",
-                class: "organ",
-                text: "人際關係糾紛的解決與化解"
-            },
-        ]
+        ques: "每年農曆二月及十月的遶境活動一般民眾是否能參加?如何報名呢？",
+        ans: "一般民眾可一同參予、請洽廟公報名 (02 - 26633398)",
+        keyword: ["繞境", "遶境"]
     },
     {
-        title: "愛情",
-        href: "love",
-        contest: [
-            {
-                color: "紅色",
-                class: "red",
-                text: "祈求擁有好得另一伴"
-            },
-            {
-                color: "紅色",
-                class: "red",
-                text: "祈求愛情穩定"
-            },
-            {
-                color: "紅色",
-                class: "red",
-                text: "感情糾紛的解決與化解"
-            },
-        ]
+        ques: "請問集順廟是否有「媽祖遶境」的活動?",
+        ans: "每五年將有一次迎媽祖遶境活動、天上聖母會進駐在石碇集順廟、到時歡迎四方信徒前來參拜！",
+        keyword: ["媽祖遶境", "媽祖"]
     },
     {
-        title: "財 運",
-        href: "money",
-        contest: [
-            {
-                color: "黃色",
-                class: "yollow",
-                text: "祈求財運亨通，財富自由"
-            },
-            {
-                color: "黃色",
-                class: "yollow",
-                text: "擁有豐裕的物質生活"
-            },
-            {
-                color: "黃色",
-                class: "yollow",
-                text: "祈求投資順利"
-            },
-        ]
-    },
-    {
-        title: "健康",
-        href: "health",
-        contest: [
-            {
-                color: "藍色",
-                class: "blue",
-                text: "祈求身理及心理上的健康(包括自己及家人)"
-            },
-            {
-                color: "藍色",
-                class: "blue",
-                text: "祈求開(騎)車平安"
-            },
-            {
-                color: "藍色",
-                class: "blue",
-                text: "祈求家人安康、和睦團圓"
-            },
-            {
-                color: "藍色",
-                class: "blue",
-                text: "祈求遠離疾病與災害"
-            },
-        ]
-    },
+        ques: "我要如何祈求平安符?",
+        ans: "可先在線上選擇最適合自己的平安符，並到石碇集順廟現場購買。",
+        keyword: ["平安符"]
+    }
 ]
+
